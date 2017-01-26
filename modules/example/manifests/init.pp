a0439b15d29c561ff5836e15a0fdd8e563653945
root <root@puppetserver.zfsgoat.com>
2017-01-26 14:43:24 +0000

    init.pp - added 'my'

--- modules/example/manifests/init.pp
+++ modules/example/manifests/init.pp
@@ -2,6 +2,6 @@ class example {
  file { 'example-module': 
    ensure => file,
    path => '/tmp/example-module.txt', 
-   content => "Content from $environment module example, from manifest init.pp\n", 
+   content => "Content from my $environment module example, from manifest init.pp\n", 
  }
 }
